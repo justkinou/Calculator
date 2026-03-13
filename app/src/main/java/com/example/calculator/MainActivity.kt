a0b30ed089eb7cfd7135b153b26143f1da774699
justkinou <justkinou@proton.me>
2026-03-13 22:13:50 +0100

    add navigation system

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import com.example.calculator.ui.screens.HomeScreen.HomeScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.calculator.ui.screens.homeScreen.HomeScreen
 import com.example.calculator.ui.theme.CalculatorTheme
+import com.example.calculator.util.Routes
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,8 +17,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CalculatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
-                    padding -> HomeScreen(padding)
+                val navController = rememberNavController();
+                NavHost(
+                    navController = navController,
+                    startDestination = Routes.HOME_SCREEN,
+                ) {
+                    composable(Routes.HOME_SCREEN) {
+                        HomeScreen()
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/example/calculator/util/Routes.kt
@@ -0,0 +1,5 @@
+package com.example.calculator.util
+
+object Routes {
+    const val HOME_SCREEN = "home_screen"
+}
